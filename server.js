8c9ac2a3c5985ce3ef4c728cac0277886c0a2234
muqeem012 <160216645+muqeem012@users.noreply.github.com>
2024-04-27 18:38:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.get('/api/data', (req, res) => {
     res.json({ message: 'Hello, world!' });
   });
 
+app.listen(3000, () => {
+            console.log('You are listening on port : ', 3000)
+        })
 
-start();
 module.exports = app;
